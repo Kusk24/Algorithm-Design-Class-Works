cd116c529e6975d9b737c414cb7aa2de3f526d07
Kusk24 <winyumg2003a@gmail.com>
2026-03-08 19:50:40 +0700

    final exam last sem Added

--- 2025 Final/fair_treasure_split.py
+++ 2025 Final/fair_treasure_split.py
@@ -1,5 +1,5 @@
-# Name : Zwe Nyan Win
-# ID   : 6540179
+# Name : Win Yu Maung
+# ID   : 6612054
 # Sec  : 541
 
 n = int(input("Enter number of treasure pieces : "))
@@ -19,13 +19,21 @@ else:
         
 
    
+# Question
 
+# A pirate discovered n pieces of treasure, each having a certain value.
+# The pirate wants to divide the treasure into two groups with equal total value so that the split is fair.
+
+# Write a program that determines whether the treasure can be split fairly.
         
+# Input
+# n
+# v1 v2 v3 ... vn
 
+# n = number of treasure pieces
+# vi = value of each treasure piece
 
-
-
-
-    
-
-
+# Output
+# If the treasures can be split equally, print the values.
+# Otherwise print:
+# Can't split fairly
--- 2025 Final/venture_capital.py
+++ 2025 Final/venture_capital.py
@@ -1,9 +1,11 @@
-# Name : Zwe Nyan Win
-# ID   : 6540179
+# Name : Win Yu Maung
+# ID   : 6612054
 # Sec  : 541
 
 n, d = map(int, input().split())
+
 funds = []
+
 for _ in range(d):
     f, e = map(int, input().split())
     funds.append((f / e, e, f))
@@ -16,6 +18,34 @@ remaining_money = n
 for ratio, e, f in funds:
     shares = remaining_money // e
     total_profit += shares * f
-    remaining_money -= shares * f
+    remaining_money -= shares * e
 
 print(total_profit)
+
+
+# Question
+
+# A venture capitalist has n units of money to invest.
+# There are d investment opportunities.
+# Each investment has:
+
+# e = cost per share
+# f = profit per share
+
+# The investor may buy multiple shares of each investment.
+# Your goal is to maximize total profit.
+
+# Input
+# n d
+# f1 e1
+# f2 e2
+# ...
+# fd ed
+
+# n = total money
+# d = number of investment options
+# f = profit
+# e = cost
+
+# Output
+# Print the maximum profit possible.
